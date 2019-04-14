b53d1083fd13c593aef62055c1a8f48ccad99818
boysinbits <shubhamanandoist@gmail.com>
2019-04-14 20:30:04 +0530

    Vaibhav - 1st commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const validateLoginInput = require("../validation/login");
 
-router.get("/",()=>{console.log("Users page works")});
+router.get("/", () => {
+  console.log("Users page works");
+});
+
+// @router POST api/users/login
 
 module.exports = router;
