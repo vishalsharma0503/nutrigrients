0e1f42727c4c38800eb22050e39762465150dd6e
Vishal Sharma <thewebdevelopervishal@gmail.com>
2019-04-15 13:37:10 +0530

    Vishal 15/04/19

--- routes/validation/login.js
+++ routes/validation/login.js
@@ -3,11 +3,11 @@ const isEmpty = require("./isEmpty");
 
 module.exports = function validateLoginInput(data) {
   let errors = {};
-  data.email = !isEmpty(data.email) ? data.email : "";
-  data.password = !isEmpty(data.password) ? data.password : "";
+  // data.email = !isEmpty(data.email) ? data.email : "";
+  // data.password = !isEmpty(data.password) ? data.password : "";
 
   if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is empty";
+    errors.email = "Email is invalid";
   }
   if (Validator.isEmpty(data.email)) {
     errors.email = "Email field is required";
