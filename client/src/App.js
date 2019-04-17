5bb48f12911e006d388f30ca5152206698bae3e9
Vishal Sharma <thewebdevelopervishal@gmail.com>
2019-04-17 11:36:58 +0530

    Vishal 17/04 11:36

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import Header from "./components/Header";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Footer from './components/Footer';
+import Landing from './components/Landing';
+import Login from './components/Login';
+import Register from './components/Register';
+import './App.css';
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-        <Header />
-        <h1>Yahan mast mast sunadariyon ki image hogi</h1>
+        <Header/>
+        <Route exact path="/" component={Landing}/>
+        <div className="container">
+        <Route exact path="/register" component={Register}></Route>
+        <Route exact path="/login" component={Login}></Route></div>
+        <Footer/>
       </div>
       </Router>
     );
